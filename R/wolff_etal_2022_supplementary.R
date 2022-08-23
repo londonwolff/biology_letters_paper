ba03a8bc231ac0cac031a075594fbd3c369aac3a
London Wolff <51676248+londonwolff@users.noreply.github.com>
2022-08-22 19:55:59 -0500

    update supplementary with tables and notes

--- R/wolff_etal_2022_supplementary.R
+++ R/wolff_etal_2022_supplementary.R
@@ -6,9 +6,10 @@
 library(tidyverse)
 library(BayesFactor)
 library(bayestestR)
+library(papaja)
 library(here)
 
-# Creating visuals for Stooge Preference Graphs-----------------------------
+# Create Visuals for Stooge Preference-----------------------------
 #Analysis for individual preference per bird for the first repition
 
 #input data
@@ -184,3 +185,85 @@ ind_preference_table_2 <- df2 %>%
 ind_preference_table_2 <- ind_preference_table_2 %>%
   select(individual, female_percent, male_percent, overall_percent)
 
+#decdie if you want them in 2 tables or if you want it to be 2 large table. We talked about 1 large set up with every indivdiual birds sex and age listed. But should we maybe just do thtat for subjects. For example, make a different table with subject bird used, what experiments they were in and age, sex, etc. yes. I should make that.
+
+#Create table of factorial pairs with corresponding differences and ratios------------
+
+factorial_pairs_df <- data.frame(Pair=c("1/2","1/3","1/4","1/5","1/6","2/3","2/4","2/5","2/6","3/4","3/5","3/6","4/5","4/6","5/6"),
+                                 Ratio=c("0.50","0.33","0.25","0.20","0.17","0.67","0.50","0.40","0.33","0.75","0.60","0.50","0.80","0.67","0.83"),
+                                 Difference=c("1","2","3","4","5","1","2","3","4","1","2","3","1","2","1"))
+
+##add row from rep 2 social pairs we completed.
+
+factorial_pairs_table <- apa_table(factorial_pairs_df)
+
+
+#Creating Fixed Effect Model Selection Table---------
+
+fixed_effect_df <- data.frame(Model = c("Intercept Only Model", "Ratio Only Model","Difference Only Model", "Both Fixed Effects, No Interaction", "Both Fixed Effects, With Interaction"),
+                              Formula = c("Choice~1","Choice~Ratio","Choice~Difference","Choice~Ratio+Difference","Choice~Ratio*Difference"))
+
+
+fixed_effect_structure_table <-apa_table(fixed_effect_df)
+
+#Creating Random Effect Model Selection Table-----------
+
+random_effect_df <- data.frame(Model = c("Intercept Only Model", "Subject Only Model","Pair Only Model", "Both Subject and Pair"),
+                               Formula = c("Choice~1","Choice~(1|Subject)","Choice~(1|Pair)","Choice~(1|Subject)+(1|Pair)"))
+
+random_effect_structure_table <- apa_table(random_effect_df)
+
+#Creating Bayes Factor tables for random & fixed effects
+
+random_models <- c("(1|Subject)","(1|Pair)","(1|Subject)+(1|Pair)")
+
+fixed_models <- c( "ratio", "difference","difference + ratio", "difference * ratio")
+
+options(digits = 2)
+
+
+
+random_social_bf_df <- data.frame(Model = random_models,
+                                  AIC = c(social_random_comparison_table$AIC[2:4]),
+                                  BIC = c(social_random_comparison_table$BIC[2:4]),
+                                  BF = c(social_random_comparison_table$BF[2:4]))
+random_social_bf_df[4] <- round(random_social_bf_df[4], digits = 2)
+
+fixed_social_bf_df <- data.frame(Model = fixed_models,
+                                 AIC = c(social_model_comparison_table$AIC[2:5]),
+                                 BIC = c(social_model_comparison_table$BIC[2:5]),
+                                 BF = c(social_model_comparison_table$BF[2:5]))
+fixed_social_bf_df[4] <- round(fixed_social_bf_df[4], digits = 2)
+
+#add in 2nd replication to the above graphs and then change it so the numbers are pulling from a dataset on this page.
+
+#How to pull from other dataset? It doesn't seem feasible to pull df in and then rerun all the modeling in this script to get the numbers I need. Or should I add this table to the end of the .rcode.R file?
+
+random_social_bf_table <- apa_table(random_social_bf_df)
+
+fixed_social_bf_table <- apa_table(fixed_social_bf_df)
+
+#Subject Bird Demographic And Experiment Information--------------
+
+all_data <- read_csv(here("data/wolff_etal_2022_data.csv"))
+
+all_data <- all_data %>%
+  unite(unique_code, c(study, rep))
+
+code_summary <- all_data |>
+  group_by(unique_code, sex, subject) |>
+  summarise(n = n(),)
+
+subject_bird_info <- code_summary %>%
+  pivot_wider(names_from = unique_code, values_from = n, values_fill = 0) %>%
+  select(subject, everything())
+
+subject_bird_info$food_1 <-  ifelse(subject_bird_info$food_1 == "0", 0, 1)
+
+subject_bird_info$food_2 <-  ifelse(subject_bird_info$food_2 == "0", 0, 1)
+
+subject_bird_info$social_1 <-  ifelse(subject_bird_info$food_2 == "0", 0, 1)
+
+subject_bird_info$social_2 <-  ifelse(subject_bird_info$food_2 == "0", 0, 1)
+
+subject_bird_info_table <- apa_table(subject_bird_info)
